1f708b72837d09f6dc0c8a3e880a156ae498ee99
barrytrearty <85511553+barrytrearty@users.noreply.github.com>
2021-09-27 15:36:33 +0100

    more clear get age by date_part

--- m6-d1.sql
+++ m6-d1.sql
@@ -190,7 +190,7 @@ SELECT category, published_at FROM public.books
 SELECT COUNT(*) FROM public.books
 
 -- Select authors and order them by age (calculate age in SQL, search about it 🤓) ASC
-SELECT date_part('year', CURRENT_DATE) - birth_year as age FROM public.authors
+SELECT name, last_name, date_part('year', CURRENT_DATE) - birth_year as age FROM public.authors
 
 -- Select authors and order them by birth_year desc
 SELECT * FROM public.authors ORDER BY birth_year DESC
